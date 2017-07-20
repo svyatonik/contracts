6ce8238b608a462f5a93ecf2ce3c88ec7e6c955d
Gav <gavin@parity.io>
2017-07-20 17:50:39 +0200

    Fix typo

--- Wallet.sol
+++ Wallet.sol
@@ -298,12 +298,12 @@ contract multisig {
 
 contract creator {
 	function doCreate(uint _value, bytes _code) internal returns (address o_addr) {
-	    bool created;
+	    bool failed;
 		assembly {
 			o_addr := create(_value, add(_code, 0x20), mload(_code))
-			created := iszero(extcodesize(o_addr))
+			failed := !iszero(extcodesize(o_addr))
 		}
-		require(created);
+		require(failed);
 	}
 }
 
