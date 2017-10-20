7fe99cc226956ee0e1b996c2b3a00fff118c49c9
keorn <pczaban@gmail.com>
2017-10-20 15:22:43 +0100

    token.sol

--- FairReceipter.sol
+++ FairReceipter.sol
@@ -6,22 +6,11 @@ pragma solidity ^0.4.7;
 
 import "Owned.sol";
 import "Certifier.sol";
+import "Token.sol";
 
 contract Recorder {
 	function received(address _who, uint _value);
 	function done();
-}
-
-// ERC20 standard token interface
-contract Token {
-	event Transfer(address indexed from, address indexed to, uint256 value);
-	event Approval(address indexed owner, address indexed spender, uint256 value);
-
-	function balanceOf(address _owner) constant returns (uint256 balance);
-	function transfer(address _to, uint256 _value) returns (bool success);
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-	function approve(address _spender, uint256 _value) returns (bool success);
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 }
 
 // BasicCoin, ECR20 tokens that all belong to the owner for sending around
