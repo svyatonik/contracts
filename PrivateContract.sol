dc04945f03ee36c4db15eb4848bfc31e26877d2b
Kirill Pimenov <kirill@pimenov.cc>
2017-11-22 17:04:04 +0100

    Nonce-aware PrivateContract

--- PrivateContract.sol
+++ PrivateContract.sol
@@ -1,12 +1,21 @@
 //! BasicCoin ECR20-compliant token contract
 //! By Parity Team, 2017.
 
-pragma solidity ^0.4.17;
+pragma solidity 0.4.18;
+
 
 contract Private {
-    address[] validators;
-    bytes state;
-    bytes code;
+    address[] public validators;
+    bytes public state;
+    bytes public code;
+    uint256 public nonce;
+
+    function Private(address[] initialValidators, bytes initialCode, bytes initialState) public {
+        validators = initialValidators;
+        code = initialCode;
+        state = initialState;
+        nonce = 1;
+    }
     
     function getValidators() public constant returns (address[]) {
         return validators;
@@ -20,18 +29,13 @@ contract Private {
         return state;
     }
     
-    function setState(bytes ns, uint8[] v, bytes32[] r, bytes32[]s) public {
-        var state_hash = keccak256(ns);
+    function setState(bytes newState, uint8[] v, bytes32[] r, bytes32[] s) public {
+        var noncedStateHash = keccak256([keccak256(newState), bytes32(nonce)]);
         for (uint i = 0; i < validators.length; i++) {
-            if (ecrecover(state_hash, v[i], r[i], s[i]) != validators[i])
+            if (ecrecover(noncedStateHash, v[i], r[i], s[i]) != validators[i])
                 revert();
         }
-        state = ns;
-    }
-    
-    function Private(address[] v, bytes c, bytes s) public {
-        validators = v;
-        code = c;
-        state = s;
+        state = newState;
+        nonce = nonce + 1;
     }
 }
